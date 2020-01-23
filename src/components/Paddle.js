7094e007db6f472878fd9e7b32dcd3ac8221f31c
Claudio Garcia <57110834+Claudegarcia@users.noreply.github.com>
2020-01-23 13:09:37 -0600

    moving paddle

--- src/components/Paddle.js
+++ src/components/Paddle.js
@@ -1,6 +1,32 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./Paddle.css";
-function Paddle() {
-  return <div className="paddle" />;
+
+export default function Paddle() {
+  const [paddleX, setPaddleX] = useState(0);
+  function handleMouse(e) {
+    console.log(e.x);
+    let boundedX;
+    const offset = (window.innerWidth - 300) / 2;
+    if (e.x - offset < 0) {
+      boundedX = 0;
+    } else if (e.x - offset > 200) {
+      boundedX = 200;
+    } else {
+      boundedX = e.x - offset;
+    }
+    setPaddleX(boundedX);
+  }
+  useEffect(() => {
+    window.addEventListener("mousemove", handleMouse);
+  }, []);
+  console.log("Paddle X => ", paddleX);
+  return (
+    <div
+      className="paddle"
+      style={{
+        left: `${paddleX}px`
+        // transform: `translateX(calc(-50% + ${paddleX}px))`
+      }}
+    />
+  );
 }
-export default Paddle;
